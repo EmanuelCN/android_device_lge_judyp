e8f426ca7a7330b613180a026fb0e58cdf6b9fe0
Darren Ramen <darren.ramen@gmail.com>
2020-06-22 19:36:44 +0400

    byebye gcam go and gcam6

--- device.mk
+++ device.mk
@@ -81,14 +81,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-#GCam Go
-PRODUCT_PACKAGES += \
-    GCam
-
-#GCam 6.1
-PRODUCT_PACKAGES += \
-    GCam6
-
 #trCamera
 PRODUCT_PACKAGES += \
     GCam7
