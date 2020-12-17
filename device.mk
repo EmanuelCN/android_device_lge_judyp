c80b11a4c600d1fcfb481452f201b6a44b22a2d0
Emanuel <emanuelghub@gmail.com>
2020-12-17 11:56:46 +0400

    Add Qti Performance blobs

--- device.mk
+++ device.mk
@@ -83,5 +83,10 @@ PRODUCT_PACKAGES += \
     se_nq_extn_client \
     libchrome.vendor
 
+# QTI performance
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # common judy (sdm845)
 $(call inherit-product, device/lge/sdm845-common/sdm845.mk)
