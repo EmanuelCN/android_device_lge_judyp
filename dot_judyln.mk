e38f3b79ca3787d266580e62a0e6d92bc47f03c6
Emanuel <emanuelghub@gmail.com>
2021-08-24 13:10:43 +0400

    judyln: remove updatable apex

--- dot_judyln.mk
+++ dot_judyln.mk
@@ -23,9 +23,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Inherit from judyln device
 $(call inherit-product, device/lge/judyln/device.mk)
 
